3baaa51b989d2b77217845113429b5c80fe44a76
Sara Qasmi <saraqasmi@macbookpro.home>
2025-10-05 18:12:51 +0200

    init commit

---
+++ playgrounds/kind/README.md
@@ -0,0 +1,19 @@
+
+## Running on Linux
+
+If you are running on Linux you can run the lab immediately.
+
+## Running on macOS
+
+If you are running on macOS you will need to install https://github.com/chipmk/docker-mac-net-connect.
+
+```bash
+brew install chipmk/tap/docker-mac-net-connect
+sudo brew services start chipmk/tap/docker-mac-net-connect
+```
+
+# Execute lab
+
+To execute this lab with [devbox](https://www.jetify.com/devbox) run `devbox run setup`.
+
+Run `devbox run shutdown` to clean up resources.
---
+++ playgrounds/kind/infra-setup/setup.sh
@@ -0,0 +1,30 @@
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+curl -fsSL https://get.jetify.com/devbox | bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+curl -s https://api.github.com/repos/derailed/k9s/releases/latest \
+| grep "browser_download_url.*Linux_amd64.tar.gz" \
+| cut -d '"' -f 4 \
+| wget -qi -
+
+tar -xvf k9s_Linux_amd64.tar.gz
+
+sudo mv k9s /usr/local/bin/
+
+k9s version

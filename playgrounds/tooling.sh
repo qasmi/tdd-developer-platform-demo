17855ceed466bc57656413a2765c16434f922673
Sara Qasmi <saraqasmi@macbookpro.home>
2025-10-19 13:06:48 +0200

    fix app config

--- playgrounds/tooling.sh
+++ playgrounds/tooling.sh
@@ -28,3 +28,8 @@ tar -xvf k9s_Linux_amd64.tar.gz
 sudo mv k9s /usr/local/bin/
 
 k9s version
+
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
+sudo apt-get update && sudo apt-get install cloudflared
